030b0d6fb5f1e1b558e2ebfad660651b78002e53
Mike Chandler <mike@rmwebfx.com>
2015-08-01 09:33:10 -0700

    Oops

--- src/main/java/com/zonkware/pi4led/controller/LedController.java
+++ src/main/java/com/zonkware/pi4led/controller/LedController.java
@@ -2,7 +2,9 @@ package com.zonkware.pi4led.controller;
 
 import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class LedController {
 
     private static GpioPinDigitalOutput pin;
